03f5907966aadab0de06c52fb371fdda7d668561
TheFreeMan21 <99295030+TheFreeMan21@users.noreply.github.com>
2023-05-21 15:21:23 +0200

    Update runner.cpp

--- runner.cpp
+++ runner.cpp
@@ -8,6 +8,16 @@ int main(){
 
     InitWindow(windowWidth ,windowHeight, "Runner");
 
+    //acceleration due to gravity (pixels/frame)/frame
+    const int gravity{1};
+
+    //player properties
+    const int width{50};
+    const int height{100};
+
+    int posY{windowHeight - height};
+    int velocity{0};
+
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
@@ -15,6 +25,25 @@ int main(){
         BeginDrawing();
         ClearBackground(WHITE);
 
+        //perform ground check
+        if(posY >= windowHeight-height){
+            //rectangle on the ground
+            velocity=0;
+
+        }else{
+            //apply gravity when in air
+            velocity+=gravity;
+        }
+
+        if(IsKeyPressed(KEY_SPACE)){
+            velocity-=10;
+        }
+
+        //update position
+        posY+=velocity;
+
+        DrawRectangle(windowWidth/2,posY, width, height, BLUE);
+        
         //stop drawing
         EndDrawing();
     }
