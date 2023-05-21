25a7f69a247919f1de7379e4b3d4093e1ae8ab94
TheFreeMan21 <99295030+TheFreeMan21@users.noreply.github.com>
2023-05-21 16:04:49 +0200

    Update runner.cpp

--- runner.cpp
+++ runner.cpp
@@ -9,7 +9,7 @@ int main(){
     InitWindow(windowWidth ,windowHeight, "Runner");
 
     //acceleration due to gravity (pixels/frame)/frame
-    const int gravity{1};
+    const int gravity{1'000};
 
     //player properties
     Texture2D runner = LoadTexture("textures/scarfy.png");
@@ -22,16 +22,26 @@ int main(){
     runnerPos.x = windowWidth/2 - runnerRec.width/2;
     runnerPos.y = windowHeight - runnerRec.height;
 
+    //animation frame
+    int frame{};
+    //amount of time before we update the animation frame
+    const float updateTime{1.0/12.0};
+    float runningTime{};
+
     //Is in air?
     bool isInAir;
-    //jump velocity
-    const int jumpVel{-22};
+    //jump velocity pixels/second
+    const int jumpVel{-600};
 
     int velocity{0};
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
+
+        //delta time(time since last frame)
+        float dT{GetFrameTime()};
+
         // start drawing
         BeginDrawing();
         ClearBackground(WHITE);
@@ -44,7 +54,7 @@ int main(){
 
         }else{
             //apply gravity when in air
-            velocity+=gravity;
+            velocity+=gravity*dT;
             isInAir=true;
         }
 
@@ -54,7 +64,19 @@ int main(){
         }
 
         //update position
-        runnerPos.y+=velocity;
+        runnerPos.y+=velocity*dT;
+
+        //update running time
+        runningTime+=dT;
+        if(runningTime >= updateTime){
+            runningTime=0;
+            //update animation frame
+            runnerRec.x = frame * runnerRec.width;
+            frame++;
+            if(frame>5){
+                frame=0;
+            }
+        }
 
         DrawTextureRec(runner, runnerRec, runnerPos, WHITE);
         
